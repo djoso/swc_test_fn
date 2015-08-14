6d6df66106460a546b96143fbfd71bf062adf1ce
Jonathan A. Gross <jarthurgross@gmail.com>
2015-08-13 21:02:53 -0600

    Added the function to be tested

---
+++ normalize_rectangle.py
@@ -0,0 +1,19 @@
+def normalize_rectangle(rect):
+    '''Takes the coordinates (x0, y0, x1, y1) for a rectangle and returns the
+    coordinates for a rectangle with sides having the same ration but with the
+    lower left corner at the origin and rescaled so the longest side has unit
+    length.'''
+    x0, y0, x1, y1 = rect   # Unpack the coordinates in rect into separate
+                            # variables
+    dx = x1 - x0    # Width
+    dy = y1 - y0    # Height
+
+    if dx > dy:
+        scaled = float(dx) / dy # This calculation is incorrect, and we need to
+                                # write a test to identify this.
+        upper_x, upper_y = 1.0, scaled
+    else:
+        scaled = float(dx) / dy
+        upper_x, upper_y = scaled, 1.0
+
+    return (0, 0, upper_x, upper_y)
