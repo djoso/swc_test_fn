994d3ecffb58532115e745b215db7ee333889d71
Jonathan A. Gross <jarthurgross@gmail.com>
2015-08-13 21:30:55 -0600

    Added fahr_to_kelvin

--- my_fns.py
+++ my_fns.py
@@ -17,3 +17,8 @@ def normalize_rectangle(rect):
         upper_x, upper_y = scaled, 1.0
 
     return (0, 0, upper_x, upper_y)
+
+def fahr_to_kelvin(temp):
+    '''Takes a temperature in Fahrenheit and returns the corresponding
+    temperature in Kelvin.'''
+    return ( (temp - 32) * (5.0 / 9) ) + 273.15
